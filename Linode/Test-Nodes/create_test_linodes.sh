ac9ed09b06e72d192e9c04e80d88f754582076aa
Koffi Azouma <31811805+koffiDev@users.noreply.github.com>
2024-08-12 18:57:52 -0400

    sp

--- Linode/Test-Nodes/create_test_linodes.sh
+++ Linode/Test-Nodes/create_test_linodes.sh
@@ -7,7 +7,7 @@ regions=$(linode-cli regions list --json | jq -r '.[].id')
 TYPE="g6-nanode-1"  # Adjust as needed
 IMAGE="linode/debian11"
 LABEL="test-linode"
-ROOT_PASS="Lo6kasXhnaYho3jcFfeH7V9R3*4Yk8F_vC4aVqPP"
+ROOT_PASS="veryStrongPassWord"
 
 # Loop through each region and create a Linode
 for region in $regions; do
